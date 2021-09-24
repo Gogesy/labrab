f4199a100b1e80bec4afc60b5c1c10ca9330fc87
Gogesy <64380593+Gogesy@users.noreply.github.com>
2021-09-24 21:06:30 +0300

    rename variables

--- Source.cpp
+++ Source.cpp
@@ -3,16 +3,16 @@
 
 
 
-int vvod_c_proverv_error(int numb)
+int vvod_c_proverv_error(int index)
 {
-	int chislo,prov;
+	int number, check;
 	int errors = 0;
 
 	while (errors == 0)
 	{
-		printf("Vvedite chislo x%d: ", numb);
-		prov = scanf("%d", &chislo);
-		if (!prov)
+		printf("Vvedite chislo x%d: ", index);
+		check = scanf("%d", &number);
+		if (!check)
 		{
 			printf("ERROR. Vvedeno ne verno. Vvedite prostoe chislo\n");
 			getchar();
@@ -24,7 +24,7 @@ int vvod_c_proverv_error(int numb)
 		}
 	}
 
-	return chislo;
+	return number;
 }
 
 // подправить проверку
@@ -33,17 +33,17 @@ int vvod_c_proverv_error(int numb)
 
 int main()
 {
-	int x1, x2, x3, Sum;
+	int number1, number2, number3, Sum;
 
-	x1 = vvod_c_proverv_error(1);
+	number1 = vvod_c_proverv_error(1);
 
-	x2 = vvod_c_proverv_error(2);
+	number2 = vvod_c_proverv_error(2);
 
-	x3 = vvod_c_proverv_error(3);
+	number3 = vvod_c_proverv_error(3);
 
-	printf("Vvedenie znacheni \nx1 = %d \nx2 = %d \nx3 = %d\n", x1, x2, x3);
+	printf("Vvedenie znacheni \nx1 = %d \nx2 = %d \nx3 = %d\n", number1, number2, number3);
 	
-	Sum = x1 + x3;
+	Sum = number1 + number3;
 
 	printf("Summa x1 u x3 = %d\n", Sum);
 
